adbd2db3094c1e9873cf8e54fb0072cf2ac22771
Bikash543 <bikashyadav9869696078@gmail.com>
2026-05-23 09:32:14 +0545

    Project 1

---
+++ hex.js
@@ -0,0 +1,29 @@
+const hex = [0,1,2,3,4,5,6,7,8,9,"A","B", "C","D", "E","F"]
+
+//#f15025
+
+
+
+const color = document.querySelector(".color")
+
+const button = document.getElementById("btn")
+
+button.addEventListener('click',function(){
+    let hexcolor = '#'
+    for(let i=0;i<6;i++){
+
+        hexcolor+=hex[getRandomNum()]
+    }
+
+    color.textContent =hexcolor
+    document.body.style.backgroundColor = hexcolor
+})
+        
+function getRandomNum(){
+
+    return Math.floor(Math.random()*hex.length)
+}
+
+
+
+
